4a42f48ce4cc1c86d24457b4c98f461745b491f6
Carsten <sgtmurtaugh@gmx.de>
2018-06-22 23:07:32 +0200

    added new map methods with DateFormat param and implement default map method for String

--- src/main/java/de/ckraus/commons/mapper/ICalenderMapper.java
+++ src/main/java/de/ckraus/commons/mapper/ICalenderMapper.java
@@ -1,29 +1,50 @@
 package de.ckraus.commons.mapper;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 
+@SuppressWarnings({ "javadoc", "unused" })
 public interface ICalenderMapper<E extends Calendar>
     extends ITypeMapper<E> {
 
-    // TODO implement!!!
+    boolean DEFAULT_LENIENT = Boolean.FALSE;
+
 
     /**
      * map
-     * @param s - string to map
-     * @param bTrim - default flag for string handling
-     * @param bEmptyIsNull - default flag for empty string handling
-     * @param defaultValue - The default value
+     * @param s
+     * @param format
+     * @param defaultValue
      * @return
-     * <p>TODO
+     * <p>Delegates to {@link #map(String, DateFormat, boolean, Calendar)} with default lenient value
+     * {@link #DEFAULT_LENIENT}</p>
      */
-    @Override
     default E map(
             String s,
-            boolean bTrim,
-            boolean bEmptyIsNull,
+            DateFormat format,
             E defaultValue
     ) {
-        return defaultValue;
+        return this.map(
+                s,
+                format,
+                DEFAULT_LENIENT,
+                defaultValue
+        );
     }
+
+    /**
+     * map
+     * @param s
+     * @param format
+     * @param lenient
+     * @param defaultValue
+     * @return
+     */
+    E map(
+            String s,
+            DateFormat format,
+            boolean lenient,
+            E defaultValue
+    );
 
 }
--- src/main/java/de/ckraus/commons/mapper/IDateMapper.java
+++ src/main/java/de/ckraus/commons/mapper/IDateMapper.java
@@ -1,11 +1,19 @@
 package de.ckraus.commons.mapper;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
+@SuppressWarnings({ "javadoc", "unused" })
 public interface IDateMapper
     extends ITypeMapper<Date> {
 
-    // TODO: implement
+    boolean DEFAULT_LENIENT = Boolean.FALSE;
+
 
     /**
      * map
@@ -14,7 +22,7 @@ public interface IDateMapper
      * @param bEmptyIsNull - default flag for empty string handling
      * @param defaultValue - The default value
      * @return
-     * <p>TODO
+     * <p>Delegates to {@link #map(String, DateFormat, boolean, Date)} with {@link DateTimeFormatter#ISO_TIME}
      */
     @Override
     default Date map(
@@ -23,7 +31,73 @@ public interface IDateMapper
             boolean bEmptyIsNull,
             Date defaultValue
     ) {
-        return defaultValue;
+        return this.map(
+                this.prepareStringToMap(
+                        s,
+                        bTrim,
+                        bEmptyIsNull
+                ),
+                SimpleDateFormat.getDateInstance(),
+                DEFAULT_LENIENT,
+                defaultValue
+        );
+    }
+
+    /**
+     * map
+     * @param s
+     * @param format
+     * @param defaultValue
+     * @return
+     * <p>Delegates to {@link #map(String, DateFormat, boolean, Date)} with default lenient value
+     * {@link #DEFAULT_LENIENT}</p>
+     */
+    default Date map(
+            String s,
+            DateFormat format,
+            Date defaultValue
+    ) {
+        return this.map(
+                s,
+                format,
+                DEFAULT_LENIENT,
+                defaultValue
+        );
+    }
+
+    /**
+     * map
+     * @param s
+     * @param format
+     * @param lenient
+     * @param defaultValue
+     * @return
+     */
+    default Date map(
+            String s,
+            DateFormat format,
+            boolean lenient,
+            Date defaultValue
+    ) {
+        Date date = defaultValue;
+        String preparedString = this.prepareStringToMap(
+                s,
+                this.isTrimStrings(),
+                this.isEmptyStringNull()
+        );
+
+        if (StringUtils.isNotEmpty(preparedString)) {
+            if (null != format) {
+                try {
+                    format.setLenient(lenient);
+                    date = format.parse(preparedString);
+                }
+                catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return date;
     }
 
 }
